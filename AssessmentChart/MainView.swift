a267523cc21bccf350849bc32f4528704bdff1ad
GTYang <gtq.yang@gmail.com>
2024-02-27 11:56:04 +0800

    first commit

---
+++ AssessmentChart/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssessmentChart
+//
+//  Created by 10362 on 2024/2/27.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
