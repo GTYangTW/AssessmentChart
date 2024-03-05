6695b88d4eedd44873f899379633a5889325ee17
GTYang <gtq.yang@gmail.com>
2024-03-05 16:45:48 +0800

    refactor: PageView 切出 Chart 使用區域的畫面

--- AssessmentChart/View/MainPGVC.swift
+++ AssessmentChart/View/MainPGVC.swift
@@ -30,12 +30,9 @@ class MainPGVC: UIPageViewController {
         btn.setTitle("工程警訊查詢", for: .normal)
         return btn
     }
-    lazy var uvDiscription = BorderWithLeftBold(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 10, height: 100), color: color.customYellow)
     let color = ColorReferance()
     lazy var arrayBtn : [UIButton] = [btn1, btn2]
     let svBtn = UIStackView()
-    let svLegend = UIStackView()
-    let uv = UIView()
     var buttonUnderLine = ButtonUnderLine()
     
     init() {
@@ -53,7 +50,6 @@ class MainPGVC: UIPageViewController {
         setViewControllers([arrayVC[0]], direction: .forward, animated: true, completion: nil)
 
         setupStackBtn()
-        setupDescription()
         // Do any additional setup after loading the view.
     }
     // PageView 也能設置 constraint 但是會因為畫面切換，導致 constraint 重新計算，再創 PageView 實體的類別設置 constraint 才對
@@ -112,44 +108,6 @@ class MainPGVC: UIPageViewController {
             make.centerX.equalTo(sender.snp.centerX)
             make.centerY.equalTo(sender.snp.bottom)
         }
-    }
-    func setupDescription(){
-        let lbDiscription: UILabel = {
-            let label = UILabel()
-            label.text = "提醒您"
-            label.textColor = color.customYellow
-            label.font = UIFont.boldSystemFont(ofSize: 20)
-            return label
-        }()
-        let lbSubdiscription: UILabel = {
-            let label = UILabel()
-            label.text = "點擊長條圖可檢視各處標案不同狀態之詳細件數。"
-            label.numberOfLines = 0
-            return label
-        }()
-        uvDiscription.addSubview(lbDiscription)
-        uvDiscription.addSubview(lbSubdiscription)
-        mainVC.view.addSubview(uvDiscription)
-        uvDiscription.snp.makeConstraints { make in
-            make.top.equalTo(mainVC.view.snp.top).offset(90)
-            make.left.right.equalToSuperview()
-            make.height.equalTo(100)
-        }
-        lbDiscription.snp.makeConstraints { make in
-            make.top.left.equalToSuperview().offset(15)
-            make.height.equalTo(labelSize(label: lbDiscription).height)
-        }
-        lbSubdiscription.snp.makeConstraints { make in
-            make.top.equalTo(lbDiscription.snp.bottomMargin).offset(20)
-            make.left.right.equalToSuperview().offset(15)
-            make.height.lessThanOrEqualTo(50)
-        }
-    }
-    func labelSize(label: UILabel) -> CGSize {
-        let labelWidth = view.frame.width - 40
-        let maxSize = CGSize(width: labelWidth, height: CGFloat.greatestFiniteMagnitude)
-        let labelBounds = label.text?.boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: label.font], context: nil)
-        return labelBounds?.size ?? CGSize.zero
     }
     
     /*
--- AssessmentChart/View/MainView.swift
+++ AssessmentChart/View/MainView.swift
@@ -10,12 +10,54 @@ import SnapKit
 
 class MainView: UIViewController {
     let mainScrollview = UIScrollView()
+    lazy var uvDiscription = BorderWithLeftBold(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 10, height: 100), color: color.customYellow)
+    let color = ColorReferance()
+    let svLegend = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mainScrollview.delegate = self
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        setupDescription()
+    }
+    func setupDescription(){
+        let lbDiscription: UILabel = {
+            let label = UILabel()
+            label.text = "提醒您"
+            label.textColor = color.customYellow
+            label.font = UIFont.boldSystemFont(ofSize: 20)
+            return label
+        }()
+        let lbSubdiscription: UILabel = {
+            let label = UILabel()
+            label.text = "點擊長條圖可檢視各處標案不同狀態之詳細件數。"
+            label.numberOfLines = 0
+            return label
+        }()
+        uvDiscription.addSubview(lbDiscription)
+        uvDiscription.addSubview(lbSubdiscription)
+        view.addSubview(uvDiscription)
+        uvDiscription.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).offset(90)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(100)
+        }
+        lbDiscription.snp.makeConstraints { make in
+            make.top.left.equalToSuperview().offset(15)
+            make.height.equalTo(labelSize(label: lbDiscription).height)
+        }
+        lbSubdiscription.snp.makeConstraints { make in
+            make.top.equalTo(lbDiscription.snp.bottomMargin).offset(20)
+            make.left.right.equalToSuperview().offset(15)
+            make.height.lessThanOrEqualTo(50)
+        }
+    }
+    func labelSize(label: UILabel) -> CGSize {
+        let labelWidth = view.frame.width - 40
+        let maxSize = CGSize(width: labelWidth, height: CGFloat.greatestFiniteMagnitude)
+        let labelBounds = label.text?.boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: label.font], context: nil)
+        return labelBounds?.size ?? CGSize.zero
     }
 
 }
